a37ab823161798b297892b9fa61b55e7c6e5f0b6
Adric <adric.landman@gmail.com>
2013-11-09 20:40:06 +1100

    Error detection code and Interprater
    It all works except the heart beat.

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -1,79 +1,137 @@
+/* Motor Driver for RC Cars
+ * For RC devices using PWM signals
+ * This takes a instructions
+ *
+ * S for Steering
+ * the packet expected is
+ * "S100\n" to "S-100\n" this means 100% sterring where 100% is left and -100 is right  
+ *
+ * M is for Motor 
+ * We expect "M100\n" to "M-100\n"
+ * 100% speed to -100% We assume that at 0% we are stopped. This might be different for some
+ * RC cars
+ *
+ * H is for heartbeat. 
+ * We send a H every 1 second otherwise it will defult to 0 and stop the motor. 
+ *
+ */
+
+
+
 #include <Servo.h> 
 
-#define NANO => 1000000000;
-#define DUTY_TOP => 10;
-#define DUTY_BOT => 5.5;
-
-#define PWM_RANGE => DUTY_TOP - DUTY_BOT;
-#define FREQ => 50;
+#define FREQ 50
+#define MIN_PULSE 1000
+#define MAX_PULSE 2000
+#define PWM_RANGE (MAX_PULSE - MIN_PULSE)
 
 Servo motor;
 Servo steer;
-String dataIn[30];
+bool invalid = false;
 char mode;
+String dataIn = "";
+
+char intBuffer[12];
+
 void setup() 
 { 
-	motor.attach(9);
-	steer.attach(10);
-	Serial.begin(19200);
+	motor.attach(9,MIN_PULSE,MAX_PULSE);
+	steer.attach(10,MIN_PULSE,MAX_PULSE);
+	Serial.begin(115200);
 	Serial.println("Ready");
+
 } 
 
 void loop() {
-  
-	if ( Serial.available()) {
+
+	while ( Serial.available()) {
 		char ch = Serial.read();
-		if (ch == 'M' && dataIn.length == 0)
+
+		if (ch == 'M' && dataIn == "")
 		{
 			mode = 'M';
 		}
-		else if (ch == 'S' && dataIn.length == 0)
+		else if (ch == 'S' && dataIn == "")
 		{
 			mode = 'S';
 		}
-		else if (ch == 'H' && dataIn.length == 0)
+		else if (ch == 'H' && dataIn == "")
 		{
 			mode = 'H';
 		}
 		else if (ch == '\n')
 		{
-			Serial.print(dataIn);
-			exeCommand();
-			dataIn = "";
+			if (mode == 'H') 
+			{
+				Serial.println("HeartBeat");
+				//exeHeartbeat
+				dataIn = "";
+			}
+			else 
+			{
+				int intLength = dataIn.length() + 1;
+				dataIn.toCharArray(intBuffer, intLength);
+				int percent = atoi(intBuffer);
+				if ( !(percent <= 100 && percent >= -100)) 
+				{
+					invalid = true;
+				}
+
+				if (!invalid) 
+				{
+					Serial.print("Percent print: ");
+					Serial.println(percent);
+					exeCommand(mode, percent);
+				}
+				else 
+				{
+					Serial.println("Invalid Command\n");
+				}
+				dataIn = "";
+				invalid = false;
+			}
 		}
 		else 
 		{
-			dataIn += ch;
+			if ( (int)ch >= 48 && (int)ch <= 57)
+			{
+				dataIn += ch;
+			}
+			else 
+			{
+				invalid = true;
+			}
 		}
-		
-		
-
-
 	}
 }
 
-void exeCommand() {
+void exeCommand(char mode, int percent) {
 	if (mode == 'M')
 	{
-
+		motor.writeMicroseconds(calc_duty(percent));
+	}
+	else if (mode == 'S')
+	{
+		steer.writeMicroseconds(calc_duty(percent));
 	}
 }
 
 
-int calc_duty (float percent_duty)
+int calc_duty (int percent_cycle)
 {
-	my ($duty, $period, $percent_scale)  = (0,0,0);
-	
-	$period = ( 1 / FREQ ) * NANO;
-	if ($percent_duty >= 0 )
+	float pulse = 0;
+	float percent = abs(percent_cycle);
+	if (percent_cycle >= 0 )
 	{
-		$duty = DUTY_BOT + PWM_RANGE/2 + (PWM_RANGE/2) * (abs($percent_duty) / 100);
+		pulse = MIN_PULSE + (PWM_RANGE/2) + ((PWM_RANGE/2) * (percent / 100));
 	}
 	else 
 	{
-		$duty = DUTY_BOT + PWM_RANGE/2 - (PWM_RANGE/2) * (abs($percent_duty) / 100);
+		pulse = MIN_PULSE + (PWM_RANGE/2) - ((PWM_RANGE/2) * (percent / 100));
 	}
-	print $duty . "\n";	
-	$duty = $period - $period * ($duty/100);
-	return $duty
+	Serial.print("Pulse width: ");
+	Serial.println(pulse);
+
+	return (int) pulse;
 }
+
